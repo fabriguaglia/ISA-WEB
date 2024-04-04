61fa8179d5d6e6eb86fbcd5b9c26176efeaecb07
fabriguaglia <fabriguaglia72@gmail.com>
2024-04-04 11:45:49 -0300

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header/Header';
 import './App.css';
 import Footer from './components/Footer/Footer';
+import HomeLanding from './components/Home-Landing/HomeLanding';
 
 function App() {
   return (
     <>
     <Header />
+    <HomeLanding />
     <Footer />
     </>
   );
