01faa05ae94a5a700545b8412a348cbbec85ed0e
fabriguaglia <fabriguaglia72@gmail.com>
2024-04-08 13:54:50 -0300

    color bar and styles modifies

--- src/components/Home-Landing/HomeLanding.js
+++ src/components/Home-Landing/HomeLanding.js
@@ -5,46 +5,48 @@ import "./HomeLanding.css"
 
 function HomeLanding() {
   return (
-    <div>
-      <div id="carouselExampleCaptions" className="carousel slide">
-        <div className="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <>
+      <div className="container-fluid">
+        <div id="carouselExampleCaptions" className="carousel slide" style={{width: "100%"}}>
+          <div className="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src={EntradaFoto} className="d-block carousel-img" alt="..." style={{objectPosition: 'center center'}} />
+              <div className="carousel-caption d-none d-md-block">
+                <h5>Admisiones 2025</h5>
+                <p>Un colegio que busca promover el esfuerzo, la constancia y la autosuperación para lograr el desarrollo de personas íntegras</p>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src={SalonFoto} className="d-block carousel-img" alt="..." style={{objectPosition: 'center top'}} />
+              <div className="carousel-caption d-none d-md-block">
+                <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+              </div>
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
         </div>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src={EntradaFoto} className="d-block w-100 img-fluid" alt="..." />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Admisiones 2025</h5>
-              <p>Un colegio que busca promover el esfuerzo, la constancia y la autosuperación para lograr el desarrollo de personas íntegras</p>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img src={SalonFoto} className="d-block w-100 img-fluid" alt="..." />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
-              <p>Some representative placeholder content for the second slide.</p>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img src="..." className="d-block w-100 img-fluid" alt="..." />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              <p>Some representative placeholder content for the third slide.</p>
-            </div>
-          </div>
-        </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
       </div>
-    </div>
+      <div className="container-fluid text-center barracolores">
+        <div className="row">
+          <div className="col azul"></div>
+          <div className="col rojo"></div>
+          <div className="col azul"></div>
+          <div className="col rojo"></div>
+        </div>
+      </div>
+    </>
   );
 }
 
