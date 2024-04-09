0550a6a65a5e21aa90ebdad23b6bd7e4593e80e7
fabriguaglia <fabriguaglia72@gmail.com>
2024-04-09 09:25:11 -0300

    Landing and css changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,43 +25,48 @@ const Header = () => {
       </div>
       <nav className="navbar bg-body-tertiary">
         <div className="container-fluid">
-          <img src={SantaAnaLogo} className='isalogoheader'/>
+          <a href='/'><img src={SantaAnaLogo} className='isalogoheader'/></a>
           <button className="navbar-toggler d-block d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-            <div className="offcanvas-header">
-              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Instituto Santa Ana</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div className="offcanvas-body">
-              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">Contacto y Soporte</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <ul className="dropdown-menu">
-                    <li><a className="dropdown-item" href="#">Action</a></li>
-                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                    <li>
-                      <hr className="dropdown-divider"/>
-                    </li>
-                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                  </ul>
-                </li>
-              </ul>
-              <form className="d-flex mt-3" role="search">
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button className="btn btn-outline-success" type="submit">Search</button>
-              </form>
-            </div>
-          </div>
+  <div className="offcanvas-header">
+    <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Instituto Santa Ana</h5>
+    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div className="offcanvas-body">
+    <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+      <li className="nav-item">
+        <a className="nav-link active" aria-current="page" href="/contact">Contacto y Soporte</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Link</a>
+      </li>
+      <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Dropdown
+        </a>
+        <ul className="dropdown-menu">
+          <li><a className="dropdown-item" href="#">Action</a></li>
+          <li><a className="dropdown-item" href="#">Another action</a></li>
+          <li>
+            <hr className="dropdown-divider"/>
+          </li>
+          <li><a className="dropdown-item" href="#">Something else here</a></li>
+        </ul>
+      </li>
+    </ul>
+    <form className="d-flex mt-3" role="search">
+      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button className="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+  <div className="bg-primary text-white text-center p-1" style={{ backgroundColor: '#334779 !important', color: '#fff !important' }}>
+  Copyright © 2024 Instituto Santa Ana, todos los derechos reservados.
+</div>
+
+</div>
+
         </div>
       </nav>
     </>
--- src/components/Home-Landing/HomeLanding.js
+++ src/components/Home-Landing/HomeLanding.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import EntradaFoto from "./isaentrada.jpg"
 import SalonFoto from "./isasalon.jpg"
+import InicialFoto from "./inicialpic.png"
+import PrimarioFoto from "./primariopic.png"
+import SecundarioFoto from "./secundariopic.png"
+import TerciarioFoto from "./terciariofoto.png"
 import "./HomeLanding.css"
 import Footer from '../Footer/Footer';
 
@@ -17,9 +21,8 @@ function HomeLanding() {
             <div className="carousel-item active">
               <img src={EntradaFoto} className="d-block carousel-img" alt="..." style={{objectPosition: 'center center'}} />
               <div className="carousel-caption">
-                <h5>Admisiones 2025</h5>
-                <p>Un colegio que busca promover el esfuerzo, la constancia y la autosuperación para lograr el desarrollo de personas íntegras</p>
-                <button type="button" className="btn custom-btn">Admisiones</button>
+                <h5>Admisiones ciclo lectivo 2025</h5>
+                <p>Consultá por nuestros cupos para todos los niveles y asegurá una educación de calidad para vos y para tus hijos.</p>
               </div>
             </div>
             <div className="carousel-item">
@@ -56,10 +59,134 @@ function HomeLanding() {
           </div>
           <div className="carousel-inner">
             <div className="carousel-item active">
-              <img src={EntradaFoto} className="d-block carousel-img" alt="..." style={{objectPosition: 'center center'}} />
+              <img src={InicialFoto} className="d-block carousel-img" alt="..." style={{objectPosition: 'center center'}} />
               <div className="carousel-caption">
                 <h5>Nivel Inicial</h5>
-                <p>Un colegio que busca promover el esfuerzo, la constancia y la autosuperación para lograr el desarrollo de personas íntegras</p>
+                <p>Incentivar el proceso de estructuración del pensamiento, de la imaginación creadora, la forma de expresión personal y de comunicacion verbal y gráfica.</p>
+                <a href='/inicial'><button type="button" className="btn custom-btn">Admisiones</button></a>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src={SalonFoto} className="d-block carousel-img" alt="..." style={{objectPosition: 'center top'}} />
+              <div className="carousel-caption">
+                <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+              </div>
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptionsBottom" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptionsBottom" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+      <div className="container-fluid text-center barracolores">
+        <div className="row">
+          <div className="col azul"></div>
+          <div className="col rojo"></div>
+          <div className="col azul"></div>
+          <div className="col rojo"></div>
+        </div>
+      </div>
+      <div className="container-fluid">
+        <div id="carouselExampleCaptionsBottom" className="carousel slide" style={{width: "100%"}}>
+          <div className="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptionsBottom" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptionsBottom" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src={PrimarioFoto} className="d-block carousel-img" alt="..." style={{objectPosition: 'center center'}} />
+              <div className="carousel-caption">
+                <h5>Nivel Primario</h5>
+                <p>Incentivar el proceso de estructuración del pensamiento, de la imaginación creadora, la forma de expresión personal y de comunicacion verbal y gráfica.</p>
+                <a href='/inicial'><button type="button" className="btn custom-btn">Admisiones</button></a>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src={SalonFoto} className="d-block carousel-img" alt="..." style={{objectPosition: 'center top'}} />
+              <div className="carousel-caption">
+                <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+              </div>
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptionsBottom" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptionsBottom" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+      <div className="container-fluid text-center barracolores">
+        <div className="row">
+          <div className="col azul"></div>
+          <div className="col rojo"></div>
+          <div className="col azul"></div>
+          <div className="col rojo"></div>
+        </div>
+      </div>
+      <div className="container-fluid">
+        <div id="carouselExampleCaptionsBottom" className="carousel slide" style={{width: "100%"}}>
+          <div className="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptionsBottom" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptionsBottom" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src={SecundarioFoto} className="d-block carousel-img" alt="..." style={{objectPosition: 'center center'}} />
+              <div className="carousel-caption">
+                <h5>Nivel Secundario</h5>
+                <p>Incentivar el proceso de estructuración del pensamiento, de la imaginación creadora, la forma de expresión personal y de comunicacion verbal y gráfica.</p>
+                <a href='/inicial'><button type="button" className="btn custom-btn">Admisiones</button></a>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src={SalonFoto} className="d-block carousel-img" alt="..." style={{objectPosition: 'center top'}} />
+              <div className="carousel-caption">
+                <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+              </div>
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptionsBottom" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptionsBottom" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+      <div className="container-fluid text-center barracolores">
+        <div className="row">
+          <div className="col azul"></div>
+          <div className="col rojo"></div>
+          <div className="col azul"></div>
+          <div className="col rojo"></div>
+        </div>
+      </div>
+      <div className="container-fluid">
+        <div id="carouselExampleCaptionsBottom" className="carousel slide" style={{width: "100%"}}>
+          <div className="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptionsBottom" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptionsBottom" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src={TerciarioFoto} className="d-block carousel-img" alt="..." style={{objectPosition: 'center center'}} />
+              <div className="carousel-caption">
+                <h5>Nivel Terciario</h5>
+                <p>Incentivar el proceso de estructuración del pensamiento, de la imaginación creadora, la forma de expresión personal y de comunicacion verbal y gráfica.</p>
+                <a href='/inicial'><button type="button" className="btn custom-btn">Admisiones</button></a>
               </div>
             </div>
             <div className="carousel-item">
