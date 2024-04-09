3c739e77a3857038052ab15857a9d033bb33b109
fran3010 <francuello30@gmail.com>
2024-04-09 09:09:17 -0300

    ContactPage-modified

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,24 +1,44 @@
 import React from 'react';
+import './Contact.css'; 
 
 function Contact() {
   return (
-    <div className="soporte-container">
-      <div className="square">
-        <div className="container text-center">
+    <>
+      <div className="container text-center">
         <div className="row">
           <div className="col">
-            Column
+            <br />
+            <h4>Contactanos</h4>
           </div>
+        </div>
+      </div>
+
+      <div className="container text-center">
+        <div className="row">
           <div className="col">
-            Column
+            <form>
+              <div className="form-group">
+                <label htmlFor="nombre"></label>
+                <input type="text" id="nombre" name="nombre" placeholder="Nombre Completo" required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email"></label>
+                <input type="email" id="email" name="email" placeholder="Correo electrónico" required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="mensaje"></label>
+                <textarea id="mensaje" name="mensaje" placeholder="Mensaje" rows="5" required></textarea>
+              </div>
+              <button type="submit">Enviar</button>
+            </form>
           </div>
+          <div className="linea-vertical"></div>
           <div className="col">
             Column
           </div>
         </div>
       </div>
-      </div>
-    </div>
+    </>
   );
 }
 
