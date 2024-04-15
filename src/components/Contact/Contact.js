3c3a32951601506212cf50a709134902a859cbeb
fran3010 <francuello30@gmail.com>
2024-04-15 16:36:16 -0300

    ContactDone

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,39 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.css';
 
 function Contact() {
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí puedes agregar la lógica para enviar el formulario, como enviar una solicitud HTTP a un servidor
+
+    // Marcar el formulario como enviado
+    setFormSubmitted(true);
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6">
           <h4>Contactenos</h4>
-          <form>
-            <div className="form-group">
-              <label htmlFor="nombre">Nombre Completo:</label>
-              <input type="text" id="nombre" name="nombre" className="form-control" placeholder="Ingrese su nombre completo" required />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Correo electrónico:</label>
-              <input type="email" id="email" name="email" className="form-control" placeholder="Ingrese su correo electrónico" required />
-            </div>
-            <div className="form-group">
-              <label htmlFor="sector">Sector a consultar:</label>
-              <select id="sector" name="sector" className="form-control" required>
-                <option value="">Seleccione un sector</option>
-                <option value="Administracion">Administración</option>
-                <option value="Nivel Inicial">Nivel Inicial</option>
-                <option value="Nivel Primario">Nivel Primario</option>
-                <option value="Nivel Secundario">Nivel Secundario</option>
-                <option value="Nivel Terciario-Profesorado">Nivel Terciario-Profesorado</option>
-                <option value="Nivel Universitario">Nivel Universitario</option>
-              </select>
-            </div>
-            <div className="form-group">
-              <label htmlFor="mensaje">Mensaje:</label>
-              <textarea id="mensaje" name="mensaje" className="form-control" placeholder="Escriba su mensaje aquí" rows="5" required></textarea>
-            </div>
-            <button type="submit" className="btn btn-primary">Enviar</button>
-          </form>
+          {formSubmitted ? (
+            <p>¡Gracias! Su mensaje ha sido enviado correctamente.</p>
+          ) : (
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="nombre">Nombre Completo:</label>
+                <input type="text" id="nombre" name="nombre" className="form-control" placeholder="Ingrese su nombre completo" required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Correo electrónico:</label>
+                <input type="email" id="email" name="email" className="form-control" placeholder="Ingrese su correo electrónico" required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="sector">Sector a consultar:</label>
+                <select id="sector" name="sector" className="form-control" required>
+                  <option value="">Seleccione un sector</option>
+                  <option value="Administracion">Administración</option>
+                  <option value="Nivel Inicial">Nivel Inicial</option>
+                  <option value="Nivel Primario">Nivel Primario</option>
+                  <option value="Nivel Secundario">Nivel Secundario</option>
+                  <option value="Nivel Terciario-Profesorado">Nivel Terciario-Profesorado</option>
+                  <option value="Nivel Universitario">Nivel Universitario</option>
+                </select>
+              </div>
+              <div className="form-group">
+                <label htmlFor="mensaje">Mensaje:</label>
+                <textarea id="mensaje" name="mensaje" className="form-control" placeholder="Escriba su mensaje aquí" rows="5" required></textarea>
+              </div>
+              <button type="submit" className="btn btn-primary">Enviar</button>
+            </form>
+          )}
         </div>
         <div className="col-md-6">
           <h5>Enlaces útiles</h5>
