ee0b80ae5dc6a328ab7f1b8f8f42a9111e07ab84
fabriguaglia <fabriguaglia72@gmail.com>
2024-04-23 11:57:46 -0300

    Levels layout test

--- src/components/Home-Landing/HomeLanding.js
+++ src/components/Home-Landing/HomeLanding.js
@@ -18,7 +18,6 @@ import SecundarioCarr from '../../assets/SecundarioCarr/SecundarioCarr';
 import TerciarioCarr from '../../assets/TerciarioCarr/TerciarioCarr';
 
 function HomeLanding() {
-
   return (
     <>
       <div className="container-fluid">
@@ -125,11 +124,11 @@ function HomeLanding() {
       </div>
       <a class="twitter-timeline" data-lang="es" data-height="400" href="https://twitter.com/InstSantaAna?ref_src=twsrc%5Etfw">Tweets de InstSantaAna</a>
         <div className="container text-center">
-        <p className="fs-3 items-desc titleinicial items-desc text-center">Convenios con otras instituciones</p>
-        <hr className='divisor'/>
-        <div className="container text-center">
-          <img className='img-fluid img-conv' alt='...' src={ConveniosFotos}/>
-        </div>
+          <p className="fs-3 items-desc titleinicial items-desc text-center">Convenios con otras instituciones</p>
+          <hr className='divisor'/>
+            <div className="container text-center">
+              <img className='img-fluid img-conv' alt='...' src={ConveniosFotos}/>
+            </div>
       </div>
       <Footer />
     </>
--- src/components/Inicial/Inicialtest.js
+++ src/components/Inicial/Inicialtest.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Swal from 'sweetalert2'
 import "./inicial.css"
 
 
@@ -12,25 +11,21 @@ import FamilyQuestionIcon from "./familyquestionicon.png"
 import VoiceIcon from "./VoiceIcon.png"
 import ProcessIcon from "./ProcessIcon.png"
 import CaduceusIcon from "./CaduceusIcon.png"
+import PerfilAlumnoPic from "./perfilalumnopic.png"
+
 import BarraColores from '../../assets/BarraColores'
 import InicialCarr from '../../assets/InicialCarr/InicialCarr.js'
+import PrimarioCarr from '../../assets/PrimarioCarr/PrimarioCarr.js'
+import SecundarioCarr from '../../assets/SecundarioCarr/SecundarioCarr.js'
 
 function Niveles() {
-
-    const handleAdmisionesClick = () => {
-        Swal.fire({
-          icon: 'info',
-          title: 'Disculpe las molestias',
-          text: 'Página en construcción'
-        });
-      };
 
   return (
     <>
     <div>
         <BarraColores />
-        <InicialCarr />
-      <BarraColores />
+    <InicialCarr />
+        <BarraColores />
       <div>
         <p class="text-center">
             <button class="mt-2 btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
@@ -189,6 +184,165 @@ function Niveles() {
       </div>
     </div>
     <div>
+        <BarraColores />
+            <PrimarioCarr />
+        <BarraColores/>
+
+        <div>
+        <p class="text-center">
+            <button class="mt-2 btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample3" aria-expanded="false" aria-controls="collapseExample">
+                <p className='mb-0 fs-2 fuente'>Objetivos</p>
+            </button>
+          <hr className='divisor'/>  
+        </p>
+        
+        <div class="collapse" id="collapseExample3">
+            <div class="card card-body">
+            <div class="row row-cols-2 text-center">
+    <div class="col items-niveles">
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#334779" class="bi bi-mortarboard-fill" viewBox="0 0 16 16">
+            <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917z"/>
+            <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466z"/>
+        </svg>
+        <p className='items-desc'>Lograr nivel académico, orden y disciplina.</p>
+    </div>
+    <div class="col items-niveles">
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#334779" class="bi bi-people-fill" viewBox="0 0 16 16">
+            <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
+        </svg>
+        <p className='items-desc'>Asegurar el clima de unión, de coerción y de esperanza entre todos los miembros de la comunidad educativa.</p>
+    </div>
+    <div class="col items-niveles">
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#334779" class="bi bi-recycle" viewBox="0 0 16 16">
+            <path d="M9.302 1.256a1.5 1.5 0 0 0-2.604 0l-1.704 2.98a.5.5 0 0 0 .869.497l1.703-2.981a.5.5 0 0 1 .868 0l2.54 4.444-1.256-.337a.5.5 0 1 0-.26.966l2.415.647a.5.5 0 0 0 .613-.353l.647-2.415a.5.5 0 1 0-.966-.259l-.333 1.242zM2.973 7.773l-1.255.337a.5.5 0 1 1-.26-.966l2.416-.647a.5.5 0 0 1 .612.353l.647 2.415a.5.5 0 0 1-.966.259l-.333-1.242-2.545 4.454a.5.5 0 0 0 .434.748H5a.5.5 0 0 1 0 1H1.723A1.5 1.5 0 0 1 .421 12.24zm10.89 1.463a.5.5 0 1 0-.868.496l1.716 3.004a.5.5 0 0 1-.434.748h-5.57l.647-.646a.5.5 0 1 0-.708-.707l-1.5 1.5a.5.5 0 0 0 0 .707l1.5 1.5a.5.5 0 1 0 .708-.707l-.647-.647h5.57a1.5 1.5 0 0 0 1.302-2.244z"/>
+        </svg>
+        <p className='items-desc'>Favorecer que la vida de la escuela se renueve permanentemente a partir de un continuo trabajo.</p>
+    </div>
+    <div class="col items-niveles">
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#334779" class="bi bi-universal-access" viewBox="0 0 16 16">
+            <path d="M9.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0M6 5.5l-4.535-.442A.531.531 0 0 1 1.531 4H14.47a.531.531 0 0 1 .066 1.058L10 5.5V9l.452 6.42a.535.535 0 0 1-1.053.174L8.243 9.97c-.064-.252-.422-.252-.486 0l-1.156 5.624a.535.535 0 0 1-1.053-.174L6 9z"/>
+        </svg>
+        <p className='items-desc'>Educar con sentido de justicia y libertad que promueva la solidaridad y el compromiso social.</p>
+    </div>
+    <div class="col items-niveles">
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#334779" class="bi bi-box2-heart-fill" viewBox="0 0 16 16">
+            <path d="M3.75 0a1 1 0 0 0-.8.4L.1 4.2a.5.5 0 0 0-.1.3V15a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V4.5a.5.5 0 0 0-.1-.3L13.05.4a1 1 0 0 0-.8-.4zM8.5 4h6l.5.667V5H1v-.333L1.5 4h6V1h1zM8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132"/>
+        </svg>
+        <p className='items-desc'>Estimular el afecto progresivo hacia los otros, desarrollando el sentido de equidad, de respeto a la libertad y a los criterios y valores ajenos.</p>
+    </div>
+    <div class="col items-niveles">
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#334779" class="bi bi-rocket-takeoff-fill" viewBox="0 0 16 16">
+            <path d="M12.17 9.53c2.307-2.592 3.278-4.684 3.641-6.218.21-.887.214-1.58.16-2.065a3.6 3.6 0 0 0-.108-.563 2 2 0 0 0-.078-.23V.453c-.073-.164-.168-.234-.352-.295a2 2 0 0 0-.16-.045 4 4 0 0 0-.57-.093c-.49-.044-1.19-.03-2.08.188-1.536.374-3.618 1.343-6.161 3.604l-2.4.238h-.006a2.55 2.55 0 0 0-1.524.734L.15 7.17a.512.512 0 0 0 .433.868l1.896-.271c.28-.04.592.013.955.132.232.076.437.16.655.248l.203.083c.196.816.66 1.58 1.275 2.195.613.614 1.376 1.08 2.191 1.277l.082.202c.089.218.173.424.249.657.118.363.172.676.132.956l-.271 1.9a.512.512 0 0 0 .867.433l2.382-2.386c.41-.41.668-.949.732-1.526zm.11-3.699c-.797.8-1.93.961-2.528.362-.598-.6-.436-1.733.361-2.532.798-.799 1.93-.96 2.528-.361s.437 1.732-.36 2.531Z"/>
+            <path d="M5.205 10.787a7.6 7.6 0 0 0 1.804 1.352c-1.118 1.007-4.929 2.028-5.054 1.903-.126-.127.737-4.189 1.839-5.18.346.69.837 1.35 1.411 1.925"/>
+        </svg>
+        <p className='items-desc'>Desarrollar actividades culturales, deportivas, científicas tecnológicas, extra programáticas que formen parte de la currícula institucional.</p>
+    </div>
+    <div class="col items-niveles">
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#334779" class="bi bi-eyeglasses" viewBox="0 0 16 16">
+            <path d="M4 6a2 2 0 1 1 0 4 2 2 0 0 1 0-4m2.625.547a3 3 0 0 0-5.584.953H.5a.5.5 0 0 0 0 1h.541A3 3 0 0 0 7 8a1 1 0 0 1 2 0 3 3 0 0 0 5.959.5h.541a.5.5 0 0 0 0-1h-.541a3 3 0 0 0-5.584-.953A2 2 0 0 0 8 6c-.532 0-1.016.208-1.375.547M14 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0"/>
+        </svg>
+        <p className='items-desc'>Valorizar la necesidad de profesionalizar en forma creciente la docencia.</p>
+    </div>
+    <div class="col items-niveles">
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#334779" class="bi bi-person-badge" viewBox="0 0 16 16">
+            <path d="M6.5 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+            <path d="M4.5 0A2.5 2.5 0 0 0 2 2.5V14a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2.5A2.5 2.5 0 0 0 11.5 0zM3 2.5A1.5 1.5 0 0 1 4.5 1h7A1.5 1.5 0 0 1 13 2.5v10.795a4.2 4.2 0 0 0-.776-.492C11.392 12.387 10.063 12 8 12s-3.392.387-4.224.803a4.2 4.2 0 0 0-.776.492z"/>
+        </svg>
+        <p className='items-desc'>Afianzar la identificación.</p>
+    </div>
+  </div>
+            </div>
+        </div>
+      </div>
+
+      <div>
+        <p class="text-center">
+            <button class="mt-2 btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample4" aria-expanded="false" aria-controls="collapseExample">
+                <p className='mb-0 fs-2 fuente'>Perfil del Alumno</p>
+            </button>
+          <hr className='divisor'/>  
+        </p>
+        
+        <div class="collapse" id="collapseExample4">
+            <div class="card card-body p-0">
+                <img className='img-fluid' src={PerfilAlumnoPic} alt='...'/>
+            </div>
+        </div>
+      </div>
+
+      <div>
+        <p class="text-center">
+            <button class="mt-2 btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample5" aria-expanded="false" aria-controls="collapseExample">
+                <p className='mb-0 fs-2 fuente'>Propuesta de Proyecto Educativo</p>
+            </button>
+          <hr className='divisor'/>  
+        </p>
+        
+        <div class="collapse" id="collapseExample5">
+            <div class="card card-body">
+                <div class="row row-cols-2 text-center">
+                    <div class="col items-niveles">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#334779" class="bi bi-mortarboard-fill" viewBox="0 0 16 16">
+                            <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917z"/>
+                            <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466z"/>
+                        </svg>
+                        <p className='items-desc'>Lograr nivel académico, orden y disciplina.</p>
+                    </div>
+                    <div class="col items-niveles">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#334779" class="bi bi-people-fill" viewBox="0 0 16 16">
+                            <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
+                        </svg>
+                        <p className='items-desc'>Asegurar el clima de unión, de coerción y de esperanza entre todos los miembros de la comunidad educativa.</p>
+                    </div>
+                    <div class="col items-niveles">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#334779" class="bi bi-recycle" viewBox="0 0 16 16">
+                            <path d="M9.302 1.256a1.5 1.5 0 0 0-2.604 0l-1.704 2.98a.5.5 0 0 0 .869.497l1.703-2.981a.5.5 0 0 1 .868 0l2.54 4.444-1.256-.337a.5.5 0 1 0-.26.966l2.415.647a.5.5 0 0 0 .613-.353l.647-2.415a.5.5 0 1 0-.966-.259l-.333 1.242zM2.973 7.773l-1.255.337a.5.5 0 1 1-.26-.966l2.416-.647a.5.5 0 0 1 .612.353l.647 2.415a.5.5 0 0 1-.966.259l-.333-1.242-2.545 4.454a.5.5 0 0 0 .434.748H5a.5.5 0 0 1 0 1H1.723A1.5 1.5 0 0 1 .421 12.24zm10.89 1.463a.5.5 0 1 0-.868.496l1.716 3.004a.5.5 0 0 1-.434.748h-5.57l.647-.646a.5.5 0 1 0-.708-.707l-1.5 1.5a.5.5 0 0 0 0 .707l1.5 1.5a.5.5 0 1 0 .708-.707l-.647-.647h5.57a1.5 1.5 0 0 0 1.302-2.244z"/>
+                        </svg>
+                        <p className='items-desc'>Favorecer que la vida de la escuela se renueve permanentemente a partir de un continuo trabajo.</p>
+                    </div>
+                    <div class="col items-niveles">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#334779" class="bi bi-universal-access" viewBox="0 0 16 16">
+                            <path d="M9.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0M6 5.5l-4.535-.442A.531.531 0 0 1 1.531 4H14.47a.531.531 0 0 1 .066 1.058L10 5.5V9l.452 6.42a.535.535 0 0 1-1.053.174L8.243 9.97c-.064-.252-.422-.252-.486 0l-1.156 5.624a.535.535 0 0 1-1.053-.174L6 9z"/>
+                        </svg>
+                        <p className='items-desc'>Educar con sentido de justicia y libertad que promueva la solidaridad y el compromiso social.</p>
+                    </div>
+                    <div class="col items-niveles">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#334779" class="bi bi-box2-heart-fill" viewBox="0 0 16 16">
+                            <path d="M3.75 0a1 1 0 0 0-.8.4L.1 4.2a.5.5 0 0 0-.1.3V15a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V4.5a.5.5 0 0 0-.1-.3L13.05.4a1 1 0 0 0-.8-.4zM8.5 4h6l.5.667V5H1v-.333L1.5 4h6V1h1zM8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132"/>
+                        </svg>
+                        <p className='items-desc'>Estimular el afecto progresivo hacia los otros, desarrollando el sentido de equidad, de respeto a la libertad y a los criterios y valores ajenos.</p>
+                    </div>
+                    <div class="col items-niveles">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#334779" class="bi bi-rocket-takeoff-fill" viewBox="0 0 16 16">
+                            <path d="M12.17 9.53c2.307-2.592 3.278-4.684 3.641-6.218.21-.887.214-1.58.16-2.065a3.6 3.6 0 0 0-.108-.563 2 2 0 0 0-.078-.23V.453c-.073-.164-.168-.234-.352-.295a2 2 0 0 0-.16-.045 4 4 0 0 0-.57-.093c-.49-.044-1.19-.03-2.08.188-1.536.374-3.618 1.343-6.161 3.604l-2.4.238h-.006a2.55 2.55 0 0 0-1.524.734L.15 7.17a.512.512 0 0 0 .433.868l1.896-.271c.28-.04.592.013.955.132.232.076.437.16.655.248l.203.083c.196.816.66 1.58 1.275 2.195.613.614 1.376 1.08 2.191 1.277l.082.202c.089.218.173.424.249.657.118.363.172.676.132.956l-.271 1.9a.512.512 0 0 0 .867.433l2.382-2.386c.41-.41.668-.949.732-1.526zm.11-3.699c-.797.8-1.93.961-2.528.362-.598-.6-.436-1.733.361-2.532.798-.799 1.93-.96 2.528-.361s.437 1.732-.36 2.531Z"/>
+                            <path d="M5.205 10.787a7.6 7.6 0 0 0 1.804 1.352c-1.118 1.007-4.929 2.028-5.054 1.903-.126-.127.737-4.189 1.839-5.18.346.69.837 1.35 1.411 1.925"/>
+                        </svg>
+                        <p className='items-desc'>Desarrollar actividades culturales, deportivas, científicas tecnológicas, extra programáticas que formen parte de la currícula institucional.</p>
+                    </div>
+                    <div class="col items-niveles">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#334779" class="bi bi-eyeglasses" viewBox="0 0 16 16">
+                            <path d="M4 6a2 2 0 1 1 0 4 2 2 0 0 1 0-4m2.625.547a3 3 0 0 0-5.584.953H.5a.5.5 0 0 0 0 1h.541A3 3 0 0 0 7 8a1 1 0 0 1 2 0 3 3 0 0 0 5.959.5h.541a.5.5 0 0 0 0-1h-.541a3 3 0 0 0-5.584-.953A2 2 0 0 0 8 6c-.532 0-1.016.208-1.375.547M14 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0"/>
+                        </svg>
+                        <p className='items-desc'>Valorizar la necesidad de profesionalizar en forma creciente la docencia.</p>
+                    </div>
+                    <div class="col items-niveles">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#334779" class="bi bi-person-badge" viewBox="0 0 16 16">
+                            <path d="M6.5 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                            <path d="M4.5 0A2.5 2.5 0 0 0 2 2.5V14a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2.5A2.5 2.5 0 0 0 11.5 0zM3 2.5A1.5 1.5 0 0 1 4.5 1h7A1.5 1.5 0 0 1 13 2.5v10.795a4.2 4.2 0 0 0-.776-.492C11.392 12.387 10.063 12 8 12s-3.392.387-4.224.803a4.2 4.2 0 0 0-.776.492z"/>
+                        </svg>
+                        <p className='items-desc'>Afianzar la identificación.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+
+            <BarraColores />
+        <SecundarioCarr />
+            <BarraColores />
+        <div>
+            
+        </div>
 
     </div>
     </>
