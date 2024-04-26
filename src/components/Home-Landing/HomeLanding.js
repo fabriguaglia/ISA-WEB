3bc23c3ce1be2133358fe3b232740019ce2a2a11
fabriguaglia <fabriguaglia72@gmail.com>
2024-04-26 08:56:45 -0300

    Twitter Daily Update

--- src/components/Home-Landing/HomeLanding.js
+++ src/components/Home-Landing/HomeLanding.js
@@ -41,13 +41,13 @@ function HomeLanding() {
       <div className="container">
         <div className="row">
           <div className="col-lg-4">
+            <Tweet id="1783823506372808738" />
+          </div>
+          <div className="col-lg-4">
             <Tweet id="1783553069621432784" />
           </div>
           <div className="col-lg-4">
             <Tweet id="1783263484802634043" />
-          </div>
-          <div className="col-lg-4">
-            <Tweet id="1782718559640236213" />
           </div>
         </div>
       </div>
